66a1534f90c05f78edfd27d98a1ba413655ad01a
Tasos Georgoulis <tasosgeorgoulis@gmail.com>
2015-03-20 22:53:53 +0200

    Change projects#show page. Display the project picture as background-image instead of img

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,8 @@
 module ProjectsHelper
+
+    # Returns a css rule to use for the project#show page
+    def project_picture_bg
+        "background-image: url(#{@project.project_picture.image.url})"
+    end
+
 end
